e65e6814c067be2668c6f083f8284b0fc09c8385
deepcc <58009015@163.com>
2020-03-25 17:38:19 +0800

    1、添加代理

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,32 @@ let appName = process.argv
   .replace("--", "");
 let outputPath = appName; //=== "bimq" ? "bimq" : "shidai";
 console.log(appName);
+console.log(IS_PROD);
+
 module.exports = {
   productionSourceMap: false,
-  publicPath: IS_PROD ? "../" + outputPath + "/" : "./",
+  // publicPath: IS_PROD ? "../" + outputPath + "/" : "./",
+  publicPath: "",
   outputDir: "dist/" + outputPath,
+  // assetsDir: "static/" + outputPath,
   assetsDir: "static",
   lintOnSave: true,
   runtimeCompiler: true,
   pages: {
     index: {
       entry: "src/views/" + appName + "/main.js"
+    }
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "<url>",
+        ws: true,
+        changeOrigin: true
+      },
+      "/foo": {
+        target: "<other_url>"
+      }
     }
   }
 };
